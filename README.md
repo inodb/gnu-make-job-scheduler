31c8780fd14e4db7b71383afb6310230b4d8af8a
Ino de Bruijn <ino.debruijn@scilifelab.se>
2013-02-13 13:10:05 +0100

    Fixed the problem of scheduling not working with the proper id. Pattern substitution was wrong. Also cleaned up sbatch.mk a bit and added a test suite.

--- README.md
+++ README.md
@@ -10,16 +10,16 @@ My strategy works like this. Create one Makefile that has all the rules to
 create the files, then create another Makefile in the same directory with
 another name e.g. Makefile-sbatch that has the same targets and dependencies
 and includes sbatch.mk. Each rule then calls `schedule_with_deps_and_store_id`
-with the resource requirements and the command that is to be executed, which
-is just `make $@`. The `-d` parameter of sbatch is determined with the
+with the resource requirements and the command that is to be executed, which is
+just `make $@`. The `-d` parameter of sbatch is determined with the
 `schedule_with_deps_and_store_id function`. This workflow is nice because your
-sbatch commands are separate from the rules to make the actual files. To
-create the files using sbatch you would use `make -f Makefile-sbatch all` and
-if you don't want to use sbatch you just use `make all`. Seeing only the sbatch
-commands is also possible by supplying the `-n` parameter to make like `make -nf
-Makefile-sbatch`. I guess it would be fairly straightforward to make this work
-for other clusters as well but I have no experience with other scheduling
-programs. Example will follow.
+sbatch commands are separate from the rules to make the actual files. To create
+the files using sbatch you would use `make -f Makefile-sbatch all` and if you
+don't want to use sbatch you just use `make all`. Seeing only the sbatch
+commands is also possible by supplying the `-n` parameter to make like `make
+-nf Makefile-sbatch`. An example can be found in test/. I guess it would be
+fairly straightforward to make this work for other clusters as well but I have
+no experience with other scheduling programs.
 
 ### KNOWN BUGS
 Unlike standard GNU make that also checks whether a dependency is newer than
--- sbatch.mk
+++ sbatch.mk
@@ -13,6 +13,7 @@
 #   dependency if the dependency file doesn't exist yet. If it is being
 #   recreated the dependency won't be scheduled and that way the old file or
 #   some partial new output might be used.
+# - Namespaces, automated prefixing everything could be an idea
 
 # For space to comma substitution
 COMMA:= ,
@@ -20,38 +21,45 @@ EMPTY:=
 SPACE:= $(EMPTY) $(EMPTY)
 
 # Check if this is a dry run
-DRY_RUN=$(filter %n n%,$(MAKEFLAGS))
+DRY_RUN:=$(filter %n n%,$(MAKEFLAGS))
 
 # Get directory of the wrapper job script for sbatch
 DIRECTORY:=$(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 SBATCH_JOB_SCRIPT:=$(DIRECTORY)/sbatch_job
 
+#-- Variables evaluated when accessed --#
+# The dependencies of a target that don't exist yet.
+NON_EXISTENT_DEPS=$(filter-out $(wildcard $^),$^)
+# The entire sbatch command that is to be run, $1 are sbatch_options, $2 the
+# commands to be run
+SBATCH_CMD=sbatch $(SBATCH_DEP_STRING) $1 $(SBATCH_JOB_SCRIPT) $2
+# Create the sbatch like dependency string -d afterok:jobid1,afterok:jobid2
+define CREATE_SBATCH_DEP_STRING
+$(if $(or $1,$(USER_SUPPLIED_DEP_IDS)),-d $(subst $(SPACE),$(COMMA),$(foreach dep,$1 $(USER_SUPPLIED_DEP_IDS),afterok:$(dep))),)
+endef
+#-- /Variables evaluated when accessed --#
+
 # If this is not a dry run, perform the actual scheduling.
 ifeq ($(DRY_RUN),)
 # Comments in make can't be written within a define so this is for the entire
 # next code block. Call with $(call schedule_with_deps_and_store_id,sbatch_options,cmd_to_be_run)
-# NON_EXISTENT_DEPS: Determine all dependencies that don't exist yet.
-# deps: Get jobids of all dependencies from the SBATCH_DEPENDENCIES list where jobs are stored like $dep1--jobid--$jobid1 $dep2--jobid--$jobid2 $depn--jobid--$jobidn
-# SBATCH_DEP_STRING: Create the dependencies option syntax needed for sbatch -d afterok:jobid1,afterok:jobidn if there are any dependencies
-# SBATCH_CMD: the entire sbatch command that is to be run
-# SBATCH_SUBMIT: output from the sbatch command, contains job id in last word if successfull. TODO: Error checking?
+# DEP_IDS: Get jobids of all dependencies from the SBATCH_DEPENDENCIES list where jobs are stored like $dep1--jobid--$jobid1 $dep2--jobid--$jobid2 $depn--jobid--$jobidn
+# SBATCH_DEP_STRING: Create the dependencies option syntax needed for sbatch
+# SBATCH_SUBMIT: output from the sbatch command, contains job id in last word if successfull.
 define schedule_with_deps_and_store_id
-$(eval NON_EXISTENT_DEPS=$(filter-out $(wildcard $^),$^))
-$(eval deps=$(foreach dep,$(NON_EXISTENT_DEPS),$(patsubst $(dep)--jobid--,,$(filter $(dep)--jobid--%,$(SBATCH_DEPENDENCIES)))))
-$(if $(or $(deps),$(USER_SUPPLIED_DEP_IDS)),$(eval SBATCH_DEP_STRING=-d $(subst $(SPACE),$(COMMA),$(foreach dep,$(deps) $(USER_SUPPLIED_DEP_IDS),afterok:$(dep)))),$(eval SBATCH_DEP_STRING=))
-$(eval SBATCH_CMD=sbatch $(SBATCH_DEP_STRING) $1 $(SBATCH_JOB_SCRIPT) $2)
+$(eval DEP_IDS=$(foreach dep,$(NON_EXISTENT_DEPS),$(patsubst $(dep)--jobid--%,%,$(filter $(dep)--jobid--%,$(SBATCH_DEPENDENCIES)))))
+$(eval SBATCH_DEP_STRING=$(call CREATE_SBATCH_DEP_STRING,$(DEP_IDS)))
+$(if $(or $(DEP_IDS),$(USER_SUPPLIED_DEP_IDS)),$(eval SBATCH_DEP_STRING=-d $(subst $(SPACE),$(COMMA),$(foreach dep,$(DEP_IDS) $(USER_SUPPLIED_DEP_IDS),afterok:$(dep)))),$(eval SBATCH_DEP_STRING=))
 @echo $(SBATCH_CMD)
-$(eval SBATCH_SUBMIT=$(shell $(SBATCH_CMD)))
+$(eval SBATCH_SUBMIT=$(shell $(SBATCH_CMD) 2>&1))
 @echo $(SBATCH_SUBMIT)
 $(eval SBATCH_DEPENDENCIES=$(SBATCH_DEPENDENCIES) $@--jobid--$(lastword $(SBATCH_SUBMIT)))
 endef
 else
-# This is a dry run, only echo command
+# This is a dry run, echo command
 # SBATCH_DEP_STRING: jobids are not known in a jobrun, except the USER_SUPPLIED_DEP_IDS, so use filenames instead for the unknowns
 define schedule_with_deps_and_store_id
-$(eval NON_EXISTENT_DEPS=$(filter-out $(wildcard $^),$^))
-$(if $(or $(NON_EXISTENT_DEPS),$(USER_SUPPLIED_DEP_IDS)),$(eval SBATCH_DEP_STRING=-d $(subst $(SPACE),$(COMMA),$(foreach dep,$(NON_EXISTENT_DEPS) $(USER_SUPPLIED_DEP_IDS),afterok:$(dep)))),$(eval SBATCH_DEP_STRING=))
-$(eval SBATCH_CMD=sbatch $(SBATCH_DEP_STRING) $1 $(SBATCH_JOB_SCRIPT) $2)
-@echo $(SBATCH_CMD)
+$(eval SBATCH_DEP_STRING=$(call CREATE_SBATCH_DEP_STRING,$(NON_EXISTENT_DEPS)))
+$(SBATCH_CMD)
 endef
 endif
